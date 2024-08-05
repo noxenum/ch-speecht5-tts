e286752f4f253dee7303054d097ed6a55885b7f3
Claudio Paonessa <clpao96@hotmail.com>
2024-08-05 08:03:02 +0200

    add HifiGAN vocoder

--- src/app/tts_model.py
+++ src/app/tts_model.py
@@ -2,12 +2,14 @@ import pickle
 
 import numpy as np
 import torch
-from transformers import pipeline
+from transformers import SpeechT5ForTextToSpeech, SpeechT5HifiGan, SpeechT5Processor
 
 
 class TTSModel:
     device_name = "cuda" if torch.cuda.is_available() else "cpu"
+    processor = None
     model = None
+    vocoder = None
     embeddings = None
 
     @classmethod
@@ -17,21 +19,26 @@ class TTSModel:
 
             xvector_picke_path = "/opt/ml/model/xvector/embeddings.pickle"
             model_dir = "/opt/ml/model/st5"
+            vocoder_dir = "/opt/ml/model/vocoder"
 
-            cls.model = pipeline("text-to-speech", model_dir)
+            cls.processor = SpeechT5Processor.from_pretrained(model_dir)
+            cls.model = SpeechT5ForTextToSpeech.from_pretrained(model_dir)
+            cls.vocoder = SpeechT5HifiGan.from_pretrained(vocoder_dir)
 
             with open(xvector_picke_path, "rb") as f:
                 cls.embeddings = pickle.load(f)
 
-        return cls.model, cls.embeddings
+        return cls.processor, cls.model, cls.vocoder, cls.embeddings
 
     @classmethod
     def predict(cls, speaker_id: int, text_ch: str) -> tuple[np.ndarray, int]:
-        model, embeddings = cls.get_model()
+        processor, model, vocoder, embeddings = cls.get_model()
 
         spembs = torch.tensor(embeddings[speaker_id]).unsqueeze(0)
 
         with torch.no_grad():
-            speech = model(text_ch, forward_params={"speaker_embeddings": spembs})
+            inputs = processor(text=text_ch, return_tensors="pt")
 
-        return speech["audio"], speech["sampling_rate"]
+            speech = model.generate_speech(inputs["input_ids"], spembs, vocoder=vocoder)
+
+        return speech.numpy(), 16000
